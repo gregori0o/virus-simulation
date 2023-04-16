017a98602049619236229f62b53089fda01e6a4c
gregori0o <glegeza@wp.pl>
2023-04-16 20:40:06 +0200

    create stats subfolder

---
+++ src/stats/region_stat.py
@@ -0,0 +1,65 @@
+from collections import defaultdict
+
+
+class RegionStatistic:
+    def __init__(self):
+        self.sick = 0
+        self.healthy = 0
+
+        self.sick_disease = defaultdict(lambda: 0)
+        self.immune_disease = defaultdict(lambda: 0)
+
+    def get_total(self):
+        return self.sick + self.healthy
+
+    def get_healthy(self, virus_name=None):
+        if virus_name is None:
+            return self.healthy
+        return (
+            self.healthy
+            - self.sick_disease[virus_name]
+            - self.immune_disease[virus_name]
+        )
+
+    def get_sick(self, virus_name=None):
+        if virus_name is None:
+            return self.sick
+        return self.sick_disease[virus_name]
+
+    def get_immune(self, virus_name):
+        return self.immune_disease[virus_name]
+
+    def healthy_birth(self):
+        self.healthy += 1
+
+    def sick_birth(self, virus_name):
+        self.sick += 1
+        self.sick_disease[virus_name] += 1
+
+    def death(self, agent):
+        if agent.is_agent_sick():
+            self.sick -= 1
+            for virus_name in agent.sick_info.keys():
+                self.sick_disease[virus_name] -= 1
+            for virus_name in agent.immunity_info.keys():
+                self.immune_disease[virus_name] -= 1
+        else:
+            self.healthy -= 1
+
+    def end_immune(self, virus_name):
+        self.immune_disease[virus_name] -= 1
+
+    def start_sick(self, virus_name):
+        self.sick_disease[virus_name] += 1
+
+    def end_sick(self, virus_name):
+        self.sick_disease[virus_name] -= 1
+        self.immune_disease[virus_name] += 1
+
+    def start_be_sick(self):
+        self.healthy -= 1
+        self.sick += 1
+
+    def end_be_sick(self):
+        self.healthy += 1
+        self.sick -= 1
